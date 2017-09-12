4e6638518bedd68bd98e6c3edea1e6c8784f8983
rlrh1996 <rlrh1996@gmail.com>
2017-09-12 10:03:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,11 +3,14 @@ playlist['Slowdive'] = 'Alison'
 playlist['My Bloody Valentine'] = 'Sometimes'
 
 function updatePlaylist(list, artistName, songTitle) {
-    return Object.assign({}, list, {[artistName]: songTitle});
+    // return Object.assign({}, list, {[artistName]: songTitle});
+    return list[artistName] = songTitle;
 }
 
 function removeFromPlaylist(list, artistName) {
-    var newObject = Object.assign({}, list);
-    delete newObject.artistName;
-    return newObject;
+    //var newObject = Object.assign({}, list);
+    //delete newObject.artistName;
+    //return newObject;
+    delete list[artistName];
+    return list;
 }
