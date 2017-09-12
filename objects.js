e772c57b005c1ec175e55c24e0769e8b3290d8c5
rlrh1996 <rlrh1996@gmail.com>
2017-09-12 10:04:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,14 +1,10 @@
 var playlist = {"Taylor Swift": "Shake it Off"};
 
 function updatePlaylist(list, artistName, songTitle) {
-    // return Object.assign({}, list, {[artistName]: songTitle});
     return list[artistName] = songTitle;
 }
 
 function removeFromPlaylist(list, artistName) {
-    //var newObject = Object.assign({}, list);
-    //delete newObject.artistName;
-    //return newObject;
     delete list[artistName];
     return list;
 }
