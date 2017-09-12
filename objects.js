e0be4d082970dcc35ee59796361a3aadb4fe04ca
rlrh1996 <rlrh1996@gmail.com>
2017-09-12 09:49:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,11 @@
 var playlist = {"Taylor Swift": "Shake It Off"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+    playlist.artistName = songTitle; 
+    return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+    delete playlist.artistName;
+    return playlist;
+}
